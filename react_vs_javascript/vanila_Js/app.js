3aaee81847e0a24a8c43ec3d8cc4a3e1823da092
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-07 07:00:23 +0100

    vanila js

--- react_vs_javascript/vanila_Js/app.js
+++ react_vs_javascript/vanila_Js/app.js
@@ -1,1 +1,20 @@
 const button = document.querySelector("button");
+
+// declaring the  modal
+let modal;
+let backdrop;
+
+// adding event listerner to the button and a showModalHandler callback
+button.addEventListener("click", showModalHandler);
+
+// declaring the show modal callback
+const showModalHandler = () => {
+  if (modal) {
+    return;
+  }
+
+  modal = document.createElement("div");
+  modal.className = "modal";
+
+  const modalText = document.createElement("p");
+};
