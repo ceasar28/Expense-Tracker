af18894724a54acfd0e4e19b21784b82c31bbbfe
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-09 16:40:01 +0100

    Js refresher task

--- (JS_refresher/index.js
+++ (JS_refresher/index.js
@@ -4,5 +4,12 @@ Your task is to add the missing logic to a transformToObjects() function that tr
 
 For the provided input [1, 2, 3] the transformToObjects() function should return [{val: 1}, {val: 2}, {val: 3}].
 
-
  */
+
+function transformToObjects(numberArray) {
+  // Todo: Add your logic
+  numberArray.map((elem) => {
+    return { val: elem };
+  });
+  // should return an array of objects
+}
