dc9838feb2c9dd241c15596e0a9ef4d6a3b32b01
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-13 13:30:23 +0100

    footer jsx

---
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Beselling.js
@@ -0,0 +1,17 @@
+function Bestselling() {
+  return (
+    <div className="main">
+      <h4>Best selling comics </h4>
+      <div className="left">
+        <p>Filter by</p>
+        <p>Series</p>
+        <p>Character</p>
+        <p>Reading List</p>
+        <p>All comics</p>
+        <button>seee more</button>
+      </div>
+    </div>
+  );
+}
+
+export default Bestselling;
