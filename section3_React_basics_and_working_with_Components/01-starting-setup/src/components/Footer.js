8e3ccf4d892eb179ad645454012c3680cda9f0e4
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-13 11:20:46 +0100

    footer jsx

---
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div>
+      <div className="comicZone">
+        <div className="logo">
+          <img> </img>
+        </div>
+      </div>
+    </div>
+  );
+}
