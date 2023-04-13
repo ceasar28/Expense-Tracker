6ca521291f305878f68b5f09ecadd7440287e1e3
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-13 11:32:21 +0100

    footer jsx

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./components/ExpenseItem";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem />
+      <Footer />
     </div>
   );
 }
