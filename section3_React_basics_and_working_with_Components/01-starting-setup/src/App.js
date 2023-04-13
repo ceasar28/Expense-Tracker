a958105ba544e7b6f47ec6f029f8dd1e2dc780ca
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-13 13:32:58 +0100

    footer jsx

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -1,11 +1,12 @@
 import ExpenseItem from "./components/ExpenseItem";
 import Footer from "./components/Footer";
-import Bestselling from "./components/Beselling";
+import Bestselling from "./components/Bestselling";
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       {/* <ExpenseItem /> */}
+      <Bestselling />
       <Footer />
     </div>
   );
