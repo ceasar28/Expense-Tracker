d2d2fa22b5c29095b66a8da900e43954b3566609
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-15 22:02:57 +0100

    adding CSS

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/ExpenseItem.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ function ExpenseItem() {
   return (
     <div className="expense-item">
       <div>April 12th 2023</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
         <div className="expense-item__price">$280.65</div>
       </div>
