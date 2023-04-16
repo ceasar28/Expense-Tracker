312054c720c58bb77cdbb68be4475e8aff3a36b7
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-16 21:13:17 +0100

    adding props

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -21,6 +21,12 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
   return (
     <div>
