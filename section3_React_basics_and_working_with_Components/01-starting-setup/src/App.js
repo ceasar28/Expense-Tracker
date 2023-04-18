48d16823ae70b6bed7b5227998664e0e1fff820c
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-18 23:29:47 +0100

    adding props

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -51,6 +51,11 @@ function App() {
         amount={expenses[3].amount}
         date={expenses[3].date}
       />
+      <ExpenseItem
+        title={expenses[4].title}
+        amount={expenses[4].amount}
+        date={expenses[4].date}
+      />
     </div>
   );
 }
