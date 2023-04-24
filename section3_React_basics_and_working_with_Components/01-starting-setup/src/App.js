004f61595bc1affd770c4ad22ca67121f6b7a037
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-24 12:30:48 +0100

    using react.createElement instead of JSX

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -1,12 +1,19 @@
 // import ExpenseItem from "./components/ExpenseItem";
 // import Card from "./components/Card";
+import React from "react";
 import Expenses from "./components/Expenses";
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses />
-    </div>
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses />
+  //   </div>
+  // );
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Lets get startes"),
+    React.createElement(Expenses, {})
   );
 }
 
