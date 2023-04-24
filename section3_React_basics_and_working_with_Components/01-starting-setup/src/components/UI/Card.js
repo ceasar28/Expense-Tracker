d9a294041dba4d875fe43fefaed62c91bcbe96dd
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-24 12:49:08 +0100

    changed to arrow functions

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/UI/Card.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
   const classes = "card " + props.className; // this allows for classname to be set
   return <div className={classes}>{props.children}</div>;
-}
+};
 export default Card;
