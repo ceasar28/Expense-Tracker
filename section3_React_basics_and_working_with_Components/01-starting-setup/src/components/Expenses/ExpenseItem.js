e71a06fb9323bec01144915101f1e8aeb2fdffca
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-24 18:49:25 +0100

    adding click events

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -9,7 +9,13 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>click me</button>
+      <button
+        onClick={() => {
+          console.log("you clicked me");
+        }}
+      >
+        click me
+      </button>
     </Card>
   );
 };
