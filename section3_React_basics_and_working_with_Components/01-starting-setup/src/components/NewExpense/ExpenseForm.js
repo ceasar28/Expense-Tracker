8205715e6a60f467423ffcde3c52f00c648d3c40
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-25 12:54:13 +0100

    handling form submission to empty input fields

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -42,13 +42,21 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
     console.log(expense);
+    // empty the input fields after submission
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -56,6 +64,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -65,6 +74,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-10-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
