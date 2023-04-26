8ec1ecebfc8422eb6a08fb561f61ea8aa0f91a0c
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-26 15:14:55 +0100

    adding expense filter date

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/App.js
@@ -4,6 +4,28 @@ import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   const dataHandler = (expensedata) => {
     console.log("from App.js", expensedata);
   };
@@ -11,7 +33,7 @@ const App = () => {
     <div>
       <h2>Let's get started!</h2>
       <NewExpense getData={dataHandler} />
-      <Expenses />
+      <Expenses items={expenses} />
     </div>
   );
 
