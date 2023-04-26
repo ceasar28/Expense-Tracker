6e993ee89444741af6c84059e2f774748e3f2cb7
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-26 16:35:01 +0100

    rendering a list

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Expenses/Expenses.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Expenses/Expenses.js
@@ -18,11 +18,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
+        {props.items.map((expense) => {
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />;
+        })}
+
         <ExpenseItem
           title={props.items[1].title}
           amount={props.items[1].amount}
