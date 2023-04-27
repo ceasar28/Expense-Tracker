14efe5ad8a599819b6fdcbf9730380a5573958f0
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-04-27 13:36:39 +0100

    chart components

--- section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Chart/Chart.js
+++ section3_React_basics_and_working_with_Components/01-starting-setup/src/components/Chart/Chart.js
@@ -1,1 +1,19 @@
 import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={null}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+};
